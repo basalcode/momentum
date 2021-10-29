a09c1125ba44d79e39ff0b126c2aaf8411d0c39d
basalcode <basalcode@gmail.com>
2021-10-29 21:14:31 +0900

    Add greeting message

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginButton = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginButton.value;
-    console.log('username', username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGrettings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGrettings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGrettings(savedUsername);
+}
