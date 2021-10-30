7eba8eb77c8bbdf71f88e912d9fef7838606dc6a
basalcode <basalcode@gmail.com>
2021-10-30 13:07:01 +0900

    Add quote

--- js/quotes.js
+++ js/quotes.js
@@ -1,12 +1,23 @@
-import axios from "axios";
+const quoteText = document.querySelector("#quote span:first-child");
+const quoteAuthor = document.querySelector("#quote span:last-child");
 
 async function getQuote() {
-    const response = await axios({
-        method: 'GET',
-        url: "https://api.adviceslip.com/advice"
-    });
+    const response = await fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
 
-    console.log(response.data);
+    console.log(response);
+
+    const randomIndex = Math.floor(Math.random() * response.length);
+    const quote = response[randomIndex];
+
+    return quote;
 }
 
-getQuote();
+async function printQuote() {
+    const { text, author } = await getQuote();
+
+    quoteText.innerText = text;
+    quoteAuthor.innerText = author;
+}
+
+printQuote();
